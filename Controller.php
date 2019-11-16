c1fe9f131acd7e6890730060a5839f2516638c52
Lara Yudilla <46836681+larayudilla@users.noreply.github.com>
2019-11-16 16:57:09 +0700

    Update Controller.php

--- Controller.php
+++ Controller.php
@@ -1,50 +1,40 @@
 <?php
+
 include_once("models/Model.php");
 
 class Controller {
 	public $model;
 	
-	public function __construct()  
-    {  
-        $this->model = new Model();
-
-    } 
+	public function __construct()  {  
+		$this->model = new Model();
+        } 
 	
-	public function index()
-	{
+	public function index() {
 		require_once('views/index.php');
 	}
 
-	function save()
-	{ 
-		
-		$param = [
+	function save() { 
+		$parameter = [
 			"account_number" =>$_POST['account_number'],
 			"bank_code" => $_POST['bank_code'],
 			"amount" =>$_POST['amount'],
 			"remark" =>$_POST['remark'],
 		];
 
-		$this->model->saveData($param);
-		$this->index(); //controller dikembalikan ke method index setelah selesai mengakses method ini.
+		$this->model->saveData($parameter);
+		$this->index(); 
 	}
 
-	function show_data()
-	{
-		if(!isset($_GET['i']))
-		{
-			//jika tidak ada parameter id yang dikirim, maka akan menampilkan semua data yang ada
-			$rs = $this->model->lihatData();
+	function show_data() {
+		if(!isset($_GET['i'])) {
+			$hsl = $this->model->lihatData();
 			require_once('views/disbursList.php');
 		}
-		else
-		{
-			//ada parameter id yang dikirim, tampilkan detail dari salah satu data yang dipilih
-			$rs = $this->model->lihatDataDetail($_GET['i']);
-			require_once('views/disbursDetail.php');
+		else {
+			$hsl = $this->model->lihatDataDetail($_GET['i']);
+    			require_once('views/disbursDetail.php');
 		}
 	}
-
 }
 
 ?>
